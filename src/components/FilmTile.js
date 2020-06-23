0e3fa08f50d6f237c90531d6ab8eadeb2f105bba
Vivek Kaveripatnam <vkavek@gmail.com>
2020-06-23 09:31:37 +0100

    Update FilmTile Meta size

--- src/components/FilmTile.js
+++ src/components/FilmTile.js
@@ -3,14 +3,24 @@ import styled from '@emotion/styled';
 import YouTube from 'react-youtube';
 
 const FilmTileContainer = styled.div`
-  width: 100%;
-  height: 149px;
+  min-width: 100%;
+  min-height: 149px;
   display: flex;
-  box-shadow: rgba(0, 0, 0, 0.25) -1px 3px 8px 0px;
-  border: 0;
+  background-image: url(${(props) => props.bgImage});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 
   @media (min-width: ${(props) => props.theme.maxPageWidth.tablet}) {
-    height: 208px;
+    min-height: 173px;
+  }
+
+  @media (min-width: ${(props) => props.theme.maxPageWidth.desktop}) {
+    min-height: 188px;
+  }
+
+  @media (min-width: ${(props) => props.theme.maxPageWidth.wide}) {
+    min-height: 215px;
   }
 `;
 
@@ -19,19 +29,27 @@ const FilmTileMetaContainer = styled.div`
   width: 100%;
   display: none;
   transition: height 0.2s cubic-bezier(0.23, 1, 0.32, 1) 0.5s;
-  border: 0;
-
-  @media (min-width: ${(props) => props.theme.maxPageWidth.tablet}) {
-    display: flex;
-  }
+  position: relative;
 
   .youtubeContainer iframe {
-    max-height: 208px;
+    height: 147px;
     width: 100%;
-    height: 100%;
     position: absolute;
-    bottom: 2px;
+    bottom: 0;
+    right: 0;
     left: 0;
+
+    @media (min-width: ${(props) => props.theme.maxPageWidth.tablet}) {
+      height: 173px;
+    }
+
+    @media (min-width: ${(props) => props.theme.maxPageWidth.desktop}) {
+      height: 188px;
+    }
+
+    @media (min-width: ${(props) => props.theme.maxPageWidth.wide}) {
+      height: 215px;
+    }
   }
 `;
 
@@ -46,12 +64,9 @@ const FilmTileWrapper = styled.div`
     max-height 0.25s cubic-bezier(0.23, 1, 0.32, 1) 0.1s;
   margin: 0 10px;
   cursor: pointer;
-  background-image: url(${(props) => props.bgImage});
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
+
   overflow: hidden;
-  position: relative;
+  box-shadow: rgba(0, 0, 0, 0.25) -1px 3px 8px 0px;
 
   @media (min-width: ${(props) => props.theme.maxPageWidth.tablet}) {
     height: 173px;
@@ -77,21 +92,24 @@ const FilmTileWrapper = styled.div`
     transition: max-height 0.25s cubic-bezier(0.23, 1, 0.32, 1) 0s;
 
     @media (min-width: ${(props) => props.theme.maxPageWidth.tablet}) {
+      height: 346px;
       max-height: 346px;
     }
 
     @media (min-width: ${(props) => props.theme.maxPageWidth.desktop}) {
+      height: 376px;
       max-height: 376px;
     }
 
     @media (min-width: ${(props) => props.theme.maxPageWidth.wide}) {
+      height: 430px;
       max-height: 430px;
     }
 
     ${FilmTileMetaContainer} {
       transition: height 0.2s cubic-bezier(0.23, 1, 0.32, 1) 0s;
-      display: flex;
-      min-height: 100%;
+      display: block;
+      height: 100%;
     }
   }
 `;
@@ -104,9 +122,12 @@ const FilmTile = ({ film }) => {
     <FilmTileWrapper
       onMouseLeave={() => setShowMeta(false)}
       className={showMeta && 'showMeta'}
-      bgImage={`https://image.tmdb.org/t/p/w500/${backdrop_path}`}
     >
-      <FilmTileContainer />
+      <FilmTileContainer
+        bgImage={
+          backdrop_path && `https://image.tmdb.org/t/p/w500/${backdrop_path}`
+        }
+      />
       <FilmTileMetaContainer>
         {showMeta && (
           <YouTube
