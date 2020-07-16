3fc2cce2b126365f01ef1e4611491d35e4973eb9
Vivek <vkavek@gmail.com>
2020-07-16 12:43:50 +0100

    Update Tile hover style

--- src/components/FilmInnerCarousel.js
+++ src/components/FilmInnerCarousel.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useSetRecoilState } from 'recoil';
 import { v4 as uuidv4 } from 'uuid';
-import classnames from 'classnames';
 import filmShape from '@app/shapes/film';
 
 import Tile from '@components/Tile';
@@ -12,31 +11,19 @@ import styled from '@emotion/styled';
 import mediaTile from '@atoms/mediaTile.atom';
 
 const TileWrapper = styled.div`
-  margin-right: 8px;
+  padding: 0 4px;
   width: 100%;
   height: 100%;
   max-width: 260px;
   max-height: 147px;
-  transition: all 0.2s ease-in-out 0.2s;
+  transition: all 0.2s ease-in-out;
+  pointer-events: auto;
 
   &:hover {
-    transform: scale(1.5) translateX(0) !important;
-    z-index: 1;
+    transform: translate3d(0, 0, 0) !important;
 
     & ~ ${() => TileWrapper} {
-      transform: translateX(65px) !important;
-    }
-
-    &:nth-of-type(1) {
-      transform: scale(1.5) translateX(43px) !important;
-
-      & ~ ${() => TileWrapper} {
-        transform: translateX(130px) !important;
-      }
-    }
-
-    &:nth-last-of-type(1) {
-      transform: scale(1.5) translateX(-37px) !important;
+      /* transform: translate3d(8px, 0, 0) !important; */
     }
   }
 `;
@@ -52,34 +39,28 @@ const FilmInnerCarouselContainer = styled.div`
   scroll-snap-stop: always;
   scroll-margin-left: 50px;
   position: relative;
-  pointer-events: auto;
-  transition: margin 0.2s ease-in-out 0.2s;
+  pointer-events: none;
+  transition: margin 0.2s ease-in-out;
 
   &:hover {
-    &.isTileHovered {
-      margin: 0 50px;
+    margin-right: 8px;
 
-      ${TileWrapper} {
-        transform: translateX(-65px);
-
-        &.isLastHovered {
-          transform: translateX(-122px);
-        }
-      }
-    }
-
-    &.isFirstHovered {
-      margin-left: 0;
-    }
-
-    &.isLastHovered {
-      margin-right: 0;
+    ${TileWrapper} {
+      /* transform: translate3d(-8px, 0, 0); */
     }
   }
 
   &:nth-last-of-type(1) {
     padding-right: 50px;
   }
+`;
+
+const TileContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+  pointer-events: none;
 `;
 
 const MediaTiles = React.memo(({ media }) => {
@@ -94,34 +75,25 @@ const MediaTiles = React.memo(({ media }) => {
 });
 
 const FilmInnerCarousel = React.forwardRef(({ films, id }, ref) => {
-  const [isLastHovered, setIsLastHovered] = useState(false);
-  const [isFirstHovered, setIsFirstHovered] = useState(false);
-  const [isTileHovered, setisTileHovered] = useState(false);
+  const [currentHoveredTileIndex, setIndex] = useState(null);
 
   return (
     <FilmInnerCarouselContainer
       ref={ref}
       id={id}
-      className={classnames(
-        isTileHovered && 'isTileHovered',
-        isLastHovered && 'isLastHovered',
-        isFirstHovered && 'isFirstHovered'
-      )}
+      currentTileIndex={currentHoveredTileIndex}
     >
-      {films.map((film, index) => (
-        <TileWrapper
-          key={index.toString()}
-          className={isLastHovered && 'isLastHovered'}
-          onMouseEnter={() => {
-            setisTileHovered(true);
-            setIsLastHovered(index + 1 === films.length);
-            setIsFirstHovered(index === 0);
-          }}
-          onMouseLeave={() => setisTileHovered(false)}
-        >
-          <MediaTiles media={film} />
-        </TileWrapper>
-      ))}
+      <TileContainer>
+        {films.map((film, index) => (
+          <TileWrapper
+            key={index.toString()}
+            onMouseEnter={() => setIndex(index)}
+            onMouseLeave={() => setIndex(null)}
+          >
+            <MediaTiles media={film} />
+          </TileWrapper>
+        ))}
+      </TileContainer>
     </FilmInnerCarouselContainer>
   );
 });
